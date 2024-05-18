731054ad01d9a7a23532f6991045ea585ea1fec1
Ishan Rajendra <learnprog21@gmail.com>
2024-05-18 14:34:22 +0530

    Completed Modal

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-// display course details
+// modal
 
-function showCourseDetail(index) {
-    const details = document.querySelectorAll(".detail-list");
-    if (details[index].style.display == "block") {
-        details[index].style.display = "none";
-    } else {
-        details.forEach(d => {
-            d.style.display = "none";
-        })
-        details[index].style.display = "block";
-    }
+const modal = document.getElementById("modal");
+const modalBackground = document.getElementById("modal-background");
+
+const openModalBtns = document.querySelectorAll(".modal-open");
+const closeModalBtns = document.querySelectorAll(".modal-close");
+
+function openModal(e) {
+    modalBackground.classList.remove("modal-hide");
+    modal.classList.remove("modal-hide");
+    console.log("Opening Modal");
 }
+
+function closeModal() {
+    modalBackground.classList.add("modal-hide");
+    modal.classList.add("modal-hide");
+    console.log("Closing Modal");
+}
+
+closeModalBtns.forEach((e) => {
+    e.addEventListener("click", closeModal);
+});
+
+
+openModalBtns.forEach((e) => {
+    e.addEventListener("click", openModal);
+});
+
+console.log(modal, modalBackground, openModalBtns, closeModalBtns);
