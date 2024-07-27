4080c3e6b13db5c11184693341a324ac65d9523c
Ishan Rajendra <learnprog21@gmail.com>
2024-07-27 19:19:06 +0530

    Site Design 4 Verion 1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,12 @@
-import LogoBox from '../logobox/LogoBox.jsx'
 import NavDesktop from './navdesktop/NavDesktop.jsx'
 import NavMobile from './navmobile/NavMobile.jsx'
-import Logo from '../svg/Logo.jsx'
+import LogoBox from '../logobox/LogoBox.jsx'
 import './Header.css'
 
 export default function Header() {
     return (
         <header>
             <LogoBox />
-            
             <NavMobile />
             <NavDesktop />
         </header>
--- src/routes/root/App.jsx
+++ src/routes/root/App.jsx
@@ -1,10 +1,16 @@
-import { Fragment } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Fragment, useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import Header from '../../components/header/Header.jsx'
 import Footer from '../../components/footer/Footer.jsx'
 
 export default function App() {
+    const { key } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [ key ])
+
     return (
         <Fragment>
             <Header />
